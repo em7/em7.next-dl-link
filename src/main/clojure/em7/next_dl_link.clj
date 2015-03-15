e8589cfc5b629bb294f20c7ad48837e2f56f57cf
eMko <mm007.emko@gmail.com>
2015-03-15 22:48:27 +0100

    added functions to load and partition input

--- src/main/clojure/em7/next_dl_link.clj
+++ src/main/clojure/em7/next_dl_link.clj
@@ -1,7 +1,72 @@
 (ns em7.next-dl-link
+  (:require [clojure.string :as string])
   (:gen-class))
+
+; the file has following fomat:
+; some stuff
+; <empty lines>
+; ...
+; links to open
+; <empty lines>
+; stored links
+; possible <empty lines>
+;
+; the links to open (for distinct serves) should be opened,
+; then the links should be copeid to stored links and the file saved
+; the empty lines and rest of the file should be retained
+
+(def input-file "dl.txt")
+
+(defn read-all-input-lines
+  "Reads all lines from the file."
+  [filename]
+  (with-open [rdr (clojure.java.io/reader filename)]
+    (doall (line-seq rdr))))
+
+(defn partition-seq-by-empty-lines
+  "Partitions the input sequence of strings by empty strings,
+   resulting in multiple seqs of strings."
+  [coll]
+  (loop [c coll
+         prev-empty? false
+         cur-vec []
+         vecs []]
+    (if (empty? c)
+      (let [vs (conj vecs cur-vec)]
+        vs)
+
+      (if (string/blank? (first c))
+        (recur (rest c)
+               true
+               (conj cur-vec (first c))
+               vecs)
+        
+        (if prev-empty?
+          (recur (rest c)
+                 false
+                 [(first c)]
+                 (conj vecs cur-vec))
+          (recur (rest c)
+                 false
+                 (conj cur-vec (first c))
+                 vecs))))))
+
+(defn find-links-to-open
+  "Gets the seq of links to be opened. It should be the
+   last but one seq in input."
+  [colls]
+  (let [n (- (count colls) 2)]
+    (nth colls n)))
+
+(defn find-backup-links
+  "Gets the seq of backups of links. It should be the last
+   seq in input."
+  [colls]
+  (let [n (- (count colls) 1)]
+    (nth colls n)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
--- src/test/clojure/em7/next_dl_link_test.clj
+++ src/test/clojure/em7/next_dl_link_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [em7.next-dl-link :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest partition-seq-by-empty-lines-test
+  (let [data (list "AAA" "AAB" "AAC" "" "" "BAA" "BAB" "BAC" "BAD" "" "CAA" "CAB" "CAC" "")
+        spl  (partition-seq-by-empty-lines data)]  
+    (is (= (count spl) 3))
+    (is (= (count (first spl)) 5))
+    (is (= (count (second spl)) 5))
+    (is (= (count (second (rest spl))) 4))))
+
+(deftest find-links-to-open-test
+  (let [data [["A" "B" "C" "" ""]
+              ["A" ""]
+              ["l1" "l2" "l3" "l4" "" ""]
+              ["s1" "s2" "s3" ""]]
+        links (find-links-to-open data)]
+    (is (= (count links) 6))
+    (is (= (first links) "l1"))
+    (is (= (second links) "l2"))))
+
+(deftest find-backup-links-test
+  (let [data [["A" "B" "C" "" ""]
+              ["A" ""]
+              ["l1" "l2" "l3" "l4" "" ""]
+              ["s1" "s2" "s3" ""]]
+        backs (find-backup-links data)]
+    (is (= (count backs) 4))
+    (is (= (first backs) "s1"))
+    (is (= (second backs) "s2"))))
